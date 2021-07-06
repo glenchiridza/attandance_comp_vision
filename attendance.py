bac9e25471179d479b433250a6a0475b012a8cd7
glenchiridza <glenchiridz@gmail.com>
2021-07-07 01:03:14 +0200

    os path

--- attendance.py
+++ attendance.py
@@ -6,10 +6,12 @@ import os
 path = 'attandanceImages'
 images =[]
 names = []
+listing = os.listdir(path)
+print(listing)
 
 image_glen = face_recognition.load_image_file("./basic_images/glen.jpg")
-image_glen = cv2.cvtColor(image_glen, cv2.BRG2RGB)
+image_glen = cv2.cvtColor(image_glen, cv2.COLOR_BGR2RGB)
 
 image_test = face_recognition.load_image_file("./basic_images/test_glen.jpg")
-image_test = cv2.cvtColor(image_test, cv2.BRG2RGB)
+image_test = cv2.cvtColor(image_test, cv2.COLOR_BGR2RGB)
 
