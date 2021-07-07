2ab037a4408b37a1f0c794752a32f86f226f4ab2
glenchiridza <glenchiridz@gmail.com>
2021-07-07 13:01:14 +0200

    fix position

--- attendance.py
+++ attendance.py
@@ -51,9 +51,9 @@ while True:
 
             y1, x2, y2, x1 = face_locale
             y1, x2, y2, x1 = y1 *4, x2 *4, y2 *4, x1 *4
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 255), 2)
-            cv2.rectangle(frame, (x1, y2 - 20), (x2, y2), (0, 255, 255), cv2.FILLED)
-            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255, 255), 2)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 0), 2)
+            cv2.rectangle(frame, (x1, y2 - 25), (x2, y2), (0, 0, 0), cv2.FILLED)
+            cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255, 255), 2)
 
     cv2.imshow("cam_view", frame)
     cv2.waitKey(1)
